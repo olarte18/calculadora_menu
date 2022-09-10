4d0292ee25f9ac4da60ddf9a361bd31afd5b72db
olarte18 <snheider2224656@correo.uis.edu.co>
2022-09-10 07:51:08 -0500

    se agrego input en codigo python

---
+++ calculadora.py
@@ -0,0 +1,24 @@
+# calculadora con menu
+from math import log
+print ("--------------------------")
+print ("----CALCULADORA - MENU----")
+print ("--------------------------")
+
+#INPUT
+bandera=False
+x=float (input ("ingrese el valor de el número x: "))
+y= float (input("ingrese el valor de el núnmero y: "))
+
+print ("\ndame la opcion que deseas realizar: \n")
+# se despliega el meni para seleccionar la opcion que desea realiza:
+print("1. sumar (el primero más el segundo)")
+print("2. restar(el primero menos el segundo)")
+print("3. multiplicar (el primero por el segundo)")
+print("4. dividir (el primero más  segundo)")
+print("5. sumar (el primero a la potencia de el segundo)")
+print("6. sumar (el logaritmo de el primero")
+
+opcion= int(input ("digite la opcion: ")
+)
+
+
